f802fa0c5f10737af876eef5695a09372d3dca65
clems-dev-maker <clementcathala430@gmail.com>
2024-02-07 18:04:49 +0100

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AboutView from "./components/AboutView";
+import { Switch, Route } from "react-router-dom";
+import MovieView from './components/MovieView'
+import SearchView from "./components/SearchView";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    console.log(searchText, "is the search text")
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${searchText}&api_key=bce11c21155a0dd22d7b539b738a283b`)
+      .then(response => response.json())
+      .then(data => {
+        setSearchResults(data.results)
+      })
+  }, [searchText])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar searchText={searchText} setSearchText={setSearchText}/>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" component={AboutView} />
+        <Route path="/search">
+          <SearchView keyword={searchText} searchResults={searchResults} />
+        </Route>
+        <Route path="/movies/:id" component={MovieView} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/AboutView.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+
+const AboutView = () => {
+    return (
+      <>
+        <Hero text="About us"/>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2 my-5">
+              <p className="lead">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod finibus cursus. Phasellus at urna euismod, tincidunt sem at, molestie magna. Maecenas pretium iaculis sapien. Pellentesque faucibus justo sit amet tellus auctor, vehicula pretium quam commodo. Proin nunc orci, rhoncus id vulputate eget, egestas nec urna. Sed condimentum erat ut velit accumsan tincidunt. Pellentesque ac dolor sem. Fusce interdum ipsum vitae efficitur facilisis. Vivamus ac est lacus. Proin dapibus ac dui et aliquam. Morbi lacinia ligula augue, nec ultrices purus blandit a. Vivamus id rhoncus augue, sit amet vestibulum nibh. Vivamus lobortis quis quam at tristique. Phasellus pulvinar nulla semper, iaculis sapien sed, mollis lorem. Nam non efficitur risus
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+export default AboutView;
